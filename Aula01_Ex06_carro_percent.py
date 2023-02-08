a7e3553a423158a4dc1c5ec27bde0092b150b05b
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-07 22:30:40 -0300

    Create Aula01_Ex06_carro_percent.py

---
+++ Aula01_Ex06_carro_percent.py
@@ -0,0 +1,8 @@
+carro = 130000
+pDistribuidor = 28
+pImposto = 45
+
+precoDistribuidor = (carro*pDistribuidor)/100
+precoImposto = (carro*pImposto)/100
+
+print("O preço do distribuidor é R${}, e o do imposto é de R${}".format(precoDistribuidor, precoImposto))
