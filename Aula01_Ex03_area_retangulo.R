113aac32e646ec8b0e0643380b59290fec6f8a1e
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-08 20:48:13 -0300

    Create Aula01_Ex03_area_retangulo.R

---
+++ Aula01_Ex03_area_retangulo.R
@@ -0,0 +1,7 @@
+base<-as.numeric(readline(prompt="Insira o valor da base do retângulo: "))
+altura<-as.numeric(readline(prompt="Insira o valor da altura do retângulo: "))
+
+area<-base*altura
+perimetro<-(base*2)+(altura*2)
+
+print(paste("a área do retângulo é", area, "e a altura é", perimetro))
