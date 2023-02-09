3379cfae1d680b486f022876563441c1460e11a7
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-09 17:22:08 -0300

    Update Aula01_Ex06_carro_percent.py

--- Aula01_Ex06_carro_percent.py
+++ Aula01_Ex06_carro_percent.py
@@ -1,8 +1,10 @@
-carro = 130000
-pDistribuidor = 28
-pImposto = 45
+carro = float(input("Insira aqui o preço de fábrica do carro: "))
+percentDistribuidor = float(input("Qual o percentual do distribuidor? "))
+percentImposto = float(input("Qual o percentual doe imposto? "))
 
-precoDistribuidor = (carro*pDistribuidor)/100
-precoImposto = (carro*pImposto)/100
+precoDistribuidor = (carro*percentDistribuidor)/100
+precoImposto = (carro*percentImposto)/100
+custoConsum = carro + precoDistribuidor + precoImposto
 
-print("O preço do distribuidor é R${}, e o do imposto é de R${}".format(precoDistribuidor, precoImposto))
+print("O preço do distribuidor é R$ {:.2f}, e o do imposto é de R$ {:.2f}. O custo final do carro é de R$ {:.2f}".format(precoDistribuidor, precoImposto, custoConsum))
+
