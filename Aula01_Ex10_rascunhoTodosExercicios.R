150edf79c175f9a6f6bb4f9ae7c4c57329941da8
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-08 21:49:33 -0300

    Create Aula01_Ex10_rascunhoTodosExercicios.R

---
+++ Aula01_Ex10_rascunhoTodosExercicios.R
@@ -0,0 +1,70 @@
+#print(' faça o input da 1a nota ')
+#av1<-scan()
+##av1<-as.numeric(readline(prompt="Insira a primeira nota: "))
+#print(' faça o input da 2a nota ')
+#av2<-scan()
+##av2<-as.numeric(readline(prompt="Insira a segunda nota: "))
+
+#media<-(av1+av2)/2
+#print(media)
+#print(paste("a nota média foi igual a:",media))
+
+#print(' faça o input do peso ')
+#peso<-scan()
+#print(' faça o input da altura ')
+#altura<-scan()
+
+#imc<-peso/(altura**2)
+#imc<-round(imc,2)
+#print(imc)
+
+#print(' faça o input da temperatura em graus Fahrenheit ')
+#TF<-scan()
+#TC=round( (5/9*(TF-32)) ,1)
+#print(TC)
+
+#Entre com o valor de TF=100
+
+#base<-as.numeric(readline(prompt="Insira o valor da base do retângulo: "))
+#altura<-as.numeric(readline(prompt="Insira o valor da altura do retângulo: "))
+
+#area<-base*altura
+#perimetro<-(base*2)+(altura*2)
+
+#print(paste("a área do retângulo é", area, "e o perímetro é", perimetro))
+
+
+#votantes<-as.numeric(readline(prompt="Insira o total de votantes: "))
+#validos<-as.numeric(readline(prompt="Insira o total de votos válidos: "))
+#brancos<-as.numeric(readline(prompt="Insira o total de votos brancos: "))
+#nulos<-as.numeric(readline(prompt="Insira o total de votos nulos: "))
+
+#pValidos = (validos*100)/votantes
+#pBrancos = (brancos*100)/votantes
+#pNulos = (nulos*100)/votantes
+
+#print(paste("No total de ", votantes, ", tivemos ", pValidos, "% de votos válidos; com ", pBrancos, "% de votos em branco e", pNulos,"% de votos nulos"))
+
+
+#A<-10 
+#B<-20
+#C<-30
+#print(paste(A,B,C))
+
+#C<-B
+#B<-A
+#A<-C
+
+#print(paste(A,B))
+
+
+
+#carro<-as.numeric(readline(prompt="Insira o valor do carro: "))
+#percentDistribuidor<-as.numeric(readline(prompt="Insira o percentual do distribuidor: "))
+#percentImposto<-as.numeric(readline(prompt="Insira o percentual de Imposto: "))
+
+#precoDistribuidor = round((carro*percentDistribuidor)/100,2)
+#precoImposto = round((carro*percentImposto)/100,2)
+#custoConsum = round(carro + (precoDistribuidor+precoImposto),2)
+
+#print(paste("O preço a ser pago ao distribuidor é de R$", precoDistribuidor, ", e o imposto a ser pago é de R$", precoImposto, ". Seu custo ao consumidor é de R$", custoConsum))
