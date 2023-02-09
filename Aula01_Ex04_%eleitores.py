6cf13b22dc9bdac9c33633020bdd2caf3e11f684
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-09 17:11:29 -0300

    Update Aula01_Ex04_%eleitores.py

--- Aula01_Ex04_%eleitores.py
+++ Aula01_Ex04_%eleitores.py
@@ -1,7 +1,7 @@
-votantes = int(input("insira a quantidade total de votantes do município")) 
-validos = int(input("insira a quantidade de votos válidos"))
-brancos = int(input("Quantos votos foram brancos?"))
-nulos = int(input("Quantos votos foram nulos?))
+votantes = int(input("insira a quantidade total de votantes do município: ")) 
+validos = int(input("insira a quantidade de votos válidos: "))
+brancos = int(input("Quantos votos foram brancos? "))
+nulos = int(input("Quantos votos foram nulos?" ))
 
 pValidos = (validos*100)/votantes
 pBrancos = (brancos*100)/votantes
