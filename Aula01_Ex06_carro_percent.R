93688c4b4632c714d26e7d59db1846b7012e3623
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-08 21:40:24 -0300

    Update Aula01_Ex06_carro_percent.R

--- Aula01_Ex06_carro_percent.R
+++ Aula01_Ex06_carro_percent.R
@@ -4,6 +4,7 @@ percentImposto<-as.numeric(readline(prompt="Insira o percentual de Imposto: "))
 
 precoDistribuidor = (carro*percentDistribuidor)/100
 precoImposto = (carro*percentImposto)/100
+custoConsum = carro + (precoDistribuidor+precoImposto)
 
 
-print(paste("O preço a ser pago ao distribuidor é de R$", precoDistribuidor, ", e o imposto a ser pago é de R$", precoImposto))
+print(paste("O preço a ser pago ao distribuidor é de R$", precoDistribuidor, ", e o imposto a ser pago é de R$", precoImposto, ". Seu custo ao consumidor é de", custoConsum))
