d2ce9f7c186f71bf82c54e645a904727e9b41cab
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 20:00:27 -0300

    Update Aula02_Ex04_IFaninhado.R

--- Aula02_Ex04_IFaninhado.R
+++ Aula02_Ex04_IFaninhado.R
@@ -1,1 +1,14 @@
+x <- 15
 
+if (x > 10) {
+  print("valor acima de 10")
+  if (x > 20) {
+    print("valor acima também de 20!")
+  } else {
+    print("valor não acima de 20.")
+  }
+} else {
+  print(" valor abaixo de 10.")
+}
+
+#O IF aqui possui uma identação que faz a diferença.
