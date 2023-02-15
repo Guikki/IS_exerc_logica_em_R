41b129c515e2349fa45cdae045b0abe82e53e0ff
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 20:34:57 -0300

    Create Aula02_Ex07_semaforo.R

---
+++ Aula02_Ex07_semaforo.R
@@ -0,0 +1,10 @@
+cor <- as.character(readline(prompt="informe a cor: "))
+
+if (cor == "verde"){
+    print ("Siga em frente")
+} else if (cor == "laranja"){
+    print ("atenção")
+} else if (cor == "vermelho"){
+    print ("pare!")
+} else
+print ("cor inválida")
