717e2764deb1c843c3bad03101d7f5721219b440
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 20:42:22 -0300

    Update Aula02_Ex07_semaforo.R

--- Aula02_Ex07_semaforo.R
+++ Aula02_Ex07_semaforo.R
@@ -2,7 +2,7 @@ cor <- as.character(readline(prompt="informe a cor: "))
 
 if (cor == "verde"){
     print ("Siga em frente")
-} else if (cor == "laranja"){
+} else if (cor == "laranja" || cor == "amarelo" || cor = "abobora"){
     print ("atenção")
 } else if (cor == "vermelho" || cor == "vermelha"){
     print ("pare!")
