6af6e6ec3c203fc0fe58a7d300315aee901efe6a
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 20:38:09 -0300

    Update Aula02_Ex07_semaforo.R

--- Aula02_Ex07_semaforo.R
+++ Aula02_Ex07_semaforo.R
@@ -4,7 +4,7 @@ if (cor == "verde"){
     print ("Siga em frente")
 } else if (cor == "laranja"){
     print ("atenção")
-} else if (cor == "vermelho"){
+} else if (cor == "vermelho" || cor == "vermelha"){
     print ("pare!")
 } else
 print ("cor inválida")
