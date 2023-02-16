a67797a8a7d7e6b06c2fbab4d50bb7f68a9327aa
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 21:36:39 -0300

    Create Aula02_Ex99_CodigoCompleto.R

---
+++ Aula02_Ex99_CodigoCompleto.R
@@ -0,0 +1,70 @@
+# a <- 330
+# b <- 200
+# if (b > a) { 
+# print("b é maior do que a")
+# } else  
+# print("a maior que b")
+
+# x <- as.numeric(readline(prompt="insira aqui o valor que deseja: "))
+
+# if (x >= 18) {
+#   print("Idade acima de 18 anos, você tem acesso total ao bar")
+#    if (x >= 16) {
+#     print("Maior de 16 anos. Você pode entrar na balada")
+#   } else {
+#     print("Você não tem idade ainda pra entrar na balada")
+#   }
+# } else {
+#   print("Abaixo de 18 anos; você não tem acesso a bebidas alcoolicas")
+# }
+
+
+# a <- 200
+# b <- 33
+# c <- 500
+
+# if (a > b && c > a) {
+#   print("Ambas as condições são verdadeiras")
+# }
+
+
+# cor <- as.character(readline(prompt="informe a cor: "))
+
+# if (cor == "verde"){
+#     print ("Siga em frente")
+# } else if (cor == "laranja"){
+#     print ("atenção")
+# } else if (cor == "vermelho" || cor == "vermelha"){
+#     print ("pare!")
+# } else
+# print ("cor inválida")
+
+# nota <-as.numeric(readline(prompt="digite aqui a nota da prova final: "))
+
+# if (nota == 10) {
+#     print(paste("sua nota foi ", nota,". Aprovado com louvor! =D"))
+# } else if (nota >= 7 && nota < 10){
+#     print(paste("sua nota foi ", nota,". Você está aprovado! =]"))
+# } else if (nota >= 0 && nota < 7){
+#     print(paste("sua nota foi ", nota,". Você foi reprovado! =( "))
+# } else
+# print(paste("insira uma nota válida"))
+
+# nota1 <-as.numeric(readline(prompt="digite aqui a nota da prova 01: "))
+# nota2 <-as.numeric(readline(prompt="digite aqui a nota da prova 02: "))
+# nota3 <-as.numeric(readline(prompt="digite aqui a nota da prova 03: "))
+
+# media <- (nota1 + nota2+ nota3)/3
+# media <- round(media,2)
+
+
+# if (media >= 9 && media <= 10) {
+#     print(paste("sua nota foi ", media,". Conceito A!"))
+# } else if (media >= 7 && media < 9){
+#     print(paste("sua nota foi ", media,". Conceito B!"))
+# } else if (media >= 5 && media < 7){
+#     print(paste("sua nota foi ", media,". Conceito C!"))
+# } else if(media < 5 && media >= 0){
+# print(paste("sua nota foi ", media,". Conceito D!"))
+# } else
+# print("média inválida. Tente novamente.")
