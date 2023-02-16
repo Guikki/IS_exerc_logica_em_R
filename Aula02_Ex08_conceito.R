6e29c938da84a4249e6e87390f3e3bc57a5fa30f
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 21:06:31 -0300

    Create Aula02_Ex08_conceito.R

---
+++ Aula02_Ex08_conceito.R
@@ -0,0 +1,16 @@
+nota1 <-as.numeric(readline(prompt="digite aqui a nota da prova 01: "))
+nota2 <-as.numeric(readline(prompt="digite aqui a nota da prova 02: "))
+nota3 <-as.numeric(readline(prompt="digite aqui a nota da prova 03: "))
+
+media = (nota1 + nota2+ nota3)/3
+
+
+if (media >= 9) {
+    print(paste("sua nota foi ", media,". Conceito A!"))
+} else if (media >= 7 && media < 9){
+    print(paste("sua nota foi ", media,". Conceito B!"))
+} else if (media >= 5 && media < 7){
+    print(paste("sua nota foi ", media,". Conceito C!"))
+} else if(media < 5){
+print(paste("sua nota foi ", media,". Conceito D!"))
+}
