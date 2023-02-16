b4ed1992d6460f2d36a98ab9dc450473554ed017
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 21:09:57 -0300

    Update and rename Aula02_Ex08_conceito.R to Aula02_Ex09_conceito.R

--- Aula02_Ex09_conceito.R
+++ Aula02_Ex09_conceito.R
@@ -5,12 +5,13 @@ nota3 <-as.numeric(readline(prompt="digite aqui a nota da prova 03: "))
 media = (nota1 + nota2+ nota3)/3
 
 
-if (media >= 9) {
+if (media >= 9 && media <= 10) {
     print(paste("sua nota foi ", media,". Conceito A!"))
 } else if (media >= 7 && media < 9){
     print(paste("sua nota foi ", media,". Conceito B!"))
 } else if (media >= 5 && media < 7){
     print(paste("sua nota foi ", media,". Conceito C!"))
-} else if(media < 5){
+} else if(media < 5 && media >= 0){
 print(paste("sua nota foi ", media,". Conceito D!"))
-}
+} else
+print("média inválida. Tente novamente.")
